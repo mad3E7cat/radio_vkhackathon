637f14f8bc1160a8e36a1816f5478998dbe5c4ec
Alex Orphan <nuk3fluk3@gmail.com>
2016-11-25 21:03:48 +0300

    dev

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.use(express.static(__dirname + '/static')) // allows access to any file in 'public'
 app.set('port', (process.env.PORT || 5000)) // set port to 5000
 
-app.get('/', function(request, response){
-	console.log("REQUESTED: " + request)
-	response.sendFile(__dirname + '/index.html')
-});
 
-app.get('/main.js', function(request, response){
-	response.sendFile(__dirname + '/static/js/main.b1476538.js');
-});
+app.get('*', function(req, resp){
+	const request = req._parsedOriginalUrl.path
+	console.log(request)
 
-app.get('/main.css', function(request, response){
-	response.sendFile(__dirname + '/static/css/main.b52aa9ee.css');
-});
+	switch(request){
+		case '/':
+			resp.sendFile(__dirname + '/index.html')
+			break
+		case '/static/main.js':
+		console.log("sending main.js")
+			resp.sendFile(__dirname + '/static/js/main.b1476538.js')
+			break
+		case '/static/main.css':
+			console.log("sending main.css")
+			resp.sendFile(__dirname + '/static/css/main.b52aa9ee.css')
+			break
+		default:
+			resp.sendFile(__dirname + request)
+	}
+
+})
 
 // if POST comes to this unuque url, we answer with "1db0c94a"
 app.post('/DfSgTr', function(request, response){ 
